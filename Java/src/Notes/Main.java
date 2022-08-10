1515dde6eea86b22045780bd38f9a31145142aeb
ciaralynch <ciaralynchwork@gmail.com>
2022-08-10 16:46:57 +0100

    Added initial GUI and comments

--- Java/src/Notes/Main.java
+++ Java/src/Notes/Main.java
@@ -1,5 +1,17 @@
 package Notes;
-
+import javax.swing.*;
+/*
+ * Author: Ciara Lynch
+ * Description: Application for taking and viewing notes, for Java practice.
+ * 
+ * */
 public class Main {
-
+	
+	public static void main(String[] args) { 
+		// Draw GUI
+		JFrame frame = new JFrame("Notes App");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(900,600);
+		JPanel mainMenuPanel = new JPanel();
+	}
 }
